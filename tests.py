041b355336dbb0d0707fb05e6ec0ba1b2d1bfb9b
akhudikova <a.khudikova@nexters.com>
2026-04-02 16:17:14 +0400

    исправления

--- tests.py
+++ tests.py
@@ -3,11 +3,10 @@ from main import BooksCollector
 
 class TestBooksCollector:
 
-    @pytest.mark.parametrize('name', ['Гордость и предубеждение и зомби', 'Что делать, если ваш кот хочет вас убить'])
-
-    def test_add_new_book_add_two_books(self, name):
+    def test_add_new_book_add_two_books(self):
         collector = BooksCollector()
-        collector.add_new_book(name)
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
 
         assert len(collector.get_books_genre()) == 2
     
@@ -32,43 +31,31 @@ class TestBooksCollector:
         collector.set_book_genre(name, genre)
 
         assert collector.get_book_genre(name) == genre
-
-    @pytest.mark.parametrize('name, genre', [
-        ('Гордость и предубеждение и зомби', 'Ужасы'),
-        ('Оно', 'Ужасы')
-    ])
     
-    def test_get_books_with_specific_genre_existing_books(self, name, genre):
+    def test_get_books_with_specific_genre_existing_books(self):
         collector = BooksCollector()
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Оно')
-        collector.set_book_genre(name, genre)
+        collector.set_book_genre('Гордость и предубеждение и зомби', 'Ужасы')
+        collector.set_book_genre('Оно', 'Ужасы')
 
         assert collector.get_books_with_specific_genre('Ужасы') == ['Гордость и предубеждение и зомби', 'Оно']
 
-    @pytest.mark.parametrize('name, genre', [
-        ('Гордость и предубеждение и зомби', 'Ужасы'),
-        ('Экспансия', 'Фантастика')
-    ])
-
-    def test_get_books_genre_two_books_different_genre(self, name, genre):
+    def test_get_books_genre_two_books_different_genre(self):
         collector = BooksCollector()
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Экспансия')
-        collector.set_book_genre(name, genre)
+        collector.set_book_genre('Гордость и предубеждение и зомби', 'Ужасы')
+        collector.set_book_genre('Экспансия', 'Фантастика')
 
         assert collector.get_books_genre() == {'Гордость и предубеждение и зомби':'Ужасы', 'Экспансия':'Фантастика'}
-    
-    @pytest.mark.parametrize('name, genre', [
-        ('Экспансия', 'Фантастика')
-        ('Незнайка', 'Комедии')
-    ])
 
     def test_get_books_for_children_one_books_not_in_age_rating(self, name, genre):
         collector = BooksCollector()
         collector.add_new_book('Экспансия')
+        collector.set_book_genre('Экспансия', 'Фантастика')
         collector.add_new_book('Незнайка')
-        collector.set_book_genre(name, genre)
+        collector.set_book_genre('Незнайка', 'Комедии')
 
         assert collector.get_books_for_children() == ['Экспансия', 'Незнайка']
 
